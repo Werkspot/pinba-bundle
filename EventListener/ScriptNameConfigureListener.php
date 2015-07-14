1defd31ee23bfa45ae16d3e7e8c95e668518c77b
Marijn Koesen <github@koesen.nl>
2015-07-14 11:52:32 +0200

    Use the controller action for the script name

--- EventListener/ScriptNameConfigureListener.php
+++ EventListener/ScriptNameConfigureListener.php
@@ -18,6 +18,15 @@ class ScriptNameConfigureListener
             return;
         }
 
-        pinba_script_name_set($event->getRequest()->getRequestUri());
+        $controller = $event->getRequest()->attributes->get('_controller');
+
+        // When we render an assetic asset we have a url like: /assets/js/all.min_part_1_modal.core_4.js but the action
+        // is always 'assetic.controller:render', so if we use that, we'll never know what was really rendered, so
+        // in that case just use the uri so we have more insight what was rendered
+        $scriptName = $controller !== 'assetic.controller:render'
+            ? $controller
+            : $event->getRequest()->getRequestUri();
+
+        pinba_script_name_set($scriptName);
     }
 }
